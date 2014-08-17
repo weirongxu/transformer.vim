b761b7854be51f75a554a3add8460ea2e433dfce
Weirong Xu <weirongxu.raidou@gmail.com>
2014-08-18 01:22:13 +0800

    Mod: Simplify pipe.js

--- autoload/transformer/util/pipe/pipe.js
+++ autoload/transformer/util/pipe/pipe.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-var StringDecoder = require('string_decoder').StringDecoder;
-var decoder = new StringDecoder('utf8');
 var content = process.argv[2];
 var cmd = process.argv[3];
 var args = process.argv.slice(4);
@@ -11,17 +9,7 @@ var spawn = require('child_process').spawn;
 var pro = spawn(
   process.platform === "win32" ? cmd+".cmd" : cmd, args);
 
-pro.stdout.on('data', function (data) {
-  process.stdout.write(decoder.write(data));
-});
+pro.stdout.pipe(process.stdout);
+pro.stderr.pipe(process.stderr);
 
-pro.stdout.on('error', function (err) {});
-
-pro.on('error', function(err) {});
-
-pro.on('close', function(code) {});
-
-pro.stdin.on('error', function(err){});
-
-pro.stdin.write(decoder.write(fs.readFileSync(content)));
-pro.stdin.end();
+fs.createReadStream(content).pipe(pro.stdin);
